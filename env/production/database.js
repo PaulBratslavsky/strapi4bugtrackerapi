f4c833479112521259a00a93d4f40a230640430f
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-02-10 06:25:00 -0500

    edit production postgress settings

--- env/production/database.js
+++ env/production/database.js
@@ -1,25 +1,20 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+    
+    return {
+        connection: {
+            client: 'postgres',
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+                ssl: { rejectUnauthorized: false },
+            },
+            debug: false,
         },
-      },
-      options: {
-        ssl: true,
-      },
-    },
-  },
-});
+    }
+};
